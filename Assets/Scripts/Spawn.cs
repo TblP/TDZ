d168c969feceff4861e743eedba1e47e1b914976
TblP AI <vczyplenkov@mail.ru>
2021-11-07 01:55:26 +0300

    waves

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -1,32 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Spawn : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
     public GameObject[] enemyPrefabsOnMap;
-    private int i;
+    public int i;
     private int b;
+    public bool check = true;
+    public bool check2 = true;
+    private int q;
+
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
-        b = Random.Range(1, 5);
+        
     }
-
     // Update is called once per frame
     void Update()
     {
-        spawnEnemy(); 
+        if (check2)
+        {
+            b = Random.Range(1, 5); 
+            spawnEnemy();
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (check && GameObject.FindGameObjectWithTag("Enemy"))
+        {
+            changeName();
+        }
+
     }
     void spawnEnemy()
     {
         if (i <= b)
         {
             i += 1;
-            int enemyIndex = Random.Range(0, enemyPrefabs.Length);
-            Instantiate(enemyPrefabs[enemyIndex], gameObject.transform.position, enemyPrefabs[enemyIndex].transform.rotation);
-            
+            //int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+            Instantiate(enemyPrefabs[0], gameObject.transform.position, enemyPrefabs[0].transform.rotation);
         } 
+    }
+
+    void changeName()
+    {
+        enemyPrefabsOnMap = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int c = 0; c < enemyPrefabsOnMap.Length; c++)
+        {
+            q+=2;
+            enemyPrefabsOnMap[c].name = "enemy" + q;
+            time = Time.fixedTime;
+            if (TimerState == 0)
+            {
+                check2 = false;
+                check = false;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Waves.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Waves : MonoBehaviour
+{
+    public Spawn _spawn;
+    public Text _waves;
+    public int numWave;
+
+    private int maxWave = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numWave = 1;
+        _spawn = GameObject.Find("Spawner").GetComponent<Spawn>();
+        _waves = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_spawn.check == false && GameObject.FindGameObjectWithTag("Enemy") == false)
+        {
+            numWave += 1;
+            _waves.text = "Волна: " + numWave + "/" + maxWave;
+            _spawn.check = true;
+            _spawn.check2 = true;
+            _spawn.i = 0;
+        }
+    }
+}
